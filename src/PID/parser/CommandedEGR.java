32c2def137cb88a4f6fa607122c02d38f6f042a8
esraaelkholy <esraa.elkholy@hotmail.com>
2016-02-06 17:47:36 +0200

    bos ya islam

--- src/PID/parser/CommandedEGR.java
+++ src/PID/parser/CommandedEGR.java
@@ -2,7 +2,7 @@ package PID.parser;
 
 public class CommandedEGR {
 	private int CommandedEGRValue;
-	public int GetCommandedEGRValue(String hexadecimal){
+	public int GetCommandedEGRvalue(String hexadecimal){
 		CommandedEGRValue = Integer.parseInt(hexadecimal, 16);
 		CommandedEGRValue = CommandedEGRValue * 100/255;	//Percentage Output
 		return CommandedEGRValue;
