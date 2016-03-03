06c4f44fe3e0c3c82087773a7a39b330b2618db2
engislamkhaled <islamkhaled1>
2016-03-03 13:56:09 +0200

    Connected from server to angularJS

--- src/com/vis/gp/Server/ServerServlet.java
+++ src/com/vis/gp/Server/ServerServlet.java
@@ -1,11 +1,14 @@
 package com.vis.gp.Server;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.text.ParseException;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -13,13 +16,16 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+
 /**
  * Servlet implementation class ServerServlet
  */
 @WebServlet("/ServerServlet")
 public class ServerServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-	private static String host="jdbc:mysql://localhost:3306/obd-gp-dev-1", username="root", pswd="12345678";
+	private static String host="jdbc:mysql://localhost:3306/obd2-db", username="root", pswd="";
 	private static Connection con;
        
     /**
@@ -36,24 +42,62 @@ public class ServerServlet extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		//response.getWriter().append("Served at: ").append(request.getContextPath());
-		String uname = request.getParameter("username");
-		String pass = request.getParameter("password");
+		/*String uname = request.getParameter("asd");
+		String pass = request.getParameter("asds");
+		//System.out.println(request.toString());
+		//System.out.println(request.getParameterNames().toString());
 		response.getWriter().println("Validating user with username = "+uname+" and password = "+pass);
-		if( ValidateUser(uname, pass) == 1)
+		System.out.println(uname);
+		System.out.println(pass);
+		/*if( ValidateUser(uname, pass) == 1)
 			response.getWriter().append("User Found.");
 		else
-			response.getWriter().append("Unable to find user.");
+			response.getWriter().append("Unable to find user.");*/
 	}
 
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	
+	/*protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		doGet(request, response);
 
 	}
-
+	*/
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException 
+	  {
+	    StringBuffer sb = new StringBuffer();
+	 
+	    try 
+	    {
+	      BufferedReader reader = request.getReader();
+	      String line = null;
+	      while ((line = reader.readLine()) != null)
+	      {
+	        sb.append(line);
+	      }
+	    } catch (Exception e) { e.printStackTrace(); }
+	    System.out.println(sb.toString());
+	    
+	    JSONParser parser = new JSONParser();
+	    JSONObject joUser = null;
+	    try {
+			joUser = (JSONObject) parser.parse(sb.toString());
+		} catch (org.json.simple.parser.ParseException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	 
+	    String user = (String) joUser.get("mail");
+	    String pass = (String) joUser.get("pass");
+	    response.setContentType("text/html");
+	    System.out.println("A new user " + user + " has been created with password " + pass);
+	    PrintWriter out = response.getWriter();
+	    out.write("123123123");
+	    out.flush();
+	    out.close();
+	  }
 	/**
 	 * @see HttpServlet#doPut(HttpServletRequest, HttpServletResponse)
 	 */
